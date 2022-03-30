1deb87ffe3f315d2df80f37f73d8e3058d082e39
Momea Alexandru <alexandru.momea@yahoo.com>
2022-03-30 12:56:13 +0300

    week7-savebutton

--- 07 week/07 week.js
+++ 07 week/07 week.js
@@ -1,55 +1,61 @@
 (() => {
-
+  let savedTime = [];
   let hours = `00`,
-      minutes = `00`,
-      seconds = `00`,
-      chronometerDisplay = document.querySelector(`[data-chronometer]`),
-      chronometerCall
+    minutes = `00`,
+    seconds = `00`,
+    chronometerDisplay = document.querySelector(`[data-chronometer]`),
+    chronometerCall;
 
   function chronometer() {
+    seconds++;
 
-    seconds ++
-
-    if (seconds < 10) seconds = `0` + seconds
+    if (seconds < 10) seconds = `0` + seconds;
 
     if (seconds > 59) {
-      seconds = `00`
-      minutes ++
+      seconds = `00`;
+      minutes++;
 
-      if (minutes < 10) minutes = `0` + minutes
+      if (minutes < 10) minutes = `0` + minutes;
     }
 
     if (minutes > 59) {
-      minutes = `00`
-      hours ++
-      
-      if (hours < 10) hours = `0` + hours
+      minutes = `00`;
+      hours++;
+
+      if (hours < 10) hours = `0` + hours;
     }
 
-    chronometerDisplay.textContent = `${hours}:${minutes}:${seconds}`
-
+    chronometerDisplay.textContent = `${hours}:${minutes}:${seconds}`;
   }
 
   play.onclick = (event) => {
-    chronometerCall = setInterval(chronometer, 1000)
-    event.target.setAttribute(`disabled`,``)
-  }
+    chronometerCall = setInterval(chronometer, 1000);
+    event.target.setAttribute(`disabled`, ``);
+  };
 
   pause.onclick = () => {
-    clearInterval(chronometerCall)
-    play.removeAttribute(`disabled`)
-  }
+    clearInterval(chronometerCall);
+    play.removeAttribute(`disabled`);
+  };
 
-  reset.onclick = () => {
-    clearInterval(chronometerCall)
-    play.removeAttribute(`disabled`)
-    chronometerDisplay.textContent = `00:00:00`
+  const reset = (document.querySelector("#reset").onclick = () => {
+    clearInterval(chronometerCall);
+    play.removeAttribute(`disabled`);
+    chronometerDisplay.textContent = `00:00:00`;
+    //console.log(chronometerDisplay.textContent)
+    (hours = `00`), (minutes = `00`), (seconds = `00`);
+  });
 
-  
-    
-      hours = `00`,
-      minutes = `00`,
-      seconds = `00`
-  }
-  
-})()
+  const save = (document.querySelector("#save").onclick = () => {
+    savedTime.push({ time: chronometerDisplay.textContent });
+    const list = document.querySelector(".list");
+    list.innerHTML = "";
+
+    for (let i = 0; i < savedTime.length; i++) {
+      let li = document.createElement("li");
+      li.innerHTML = savedTime[i].time;
+      list.appendChild(li);
+    }
+    reset();
+  });
+})();
